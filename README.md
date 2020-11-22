d7d8ca6eecfd8c7e2b152e3aa248c8a0b5ca28c2
artyerokhin <arty.erokhin@gmail.com>
2020-11-22 16:16:47 +0300

    inital version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cv_parser
 cv text parser for hack
+
+- run parser on local host: python parse_cv.py
+- parse cv text - curl -X POST "http://127.0.0.1:8000/parse?text=your_text" -H  "accept: application/json" -d ""
---
+++ parse_cv.py
@@ -0,0 +1,130 @@
+# coding: utf-8
+
+
+import uvicorn
+from fastapi import FastAPI
+
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    NewsNERTagger,
+    PER,
+    NamesExtractor,
+    Doc,
+)
+
+import re
+import json
+
+from summa import keywords, summarizer
+from stop_words import get_stop_words
+
+
+# PHONE_REGEXP = "[\+\d]?(\d{2,3}[-\.\s]??\d{2,3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
+PHONE_REGEXP = "\+? {0,2}\d+ {0,2}[(-]?\d(?:[ \d]*\d)?[)-]? {0,2}\d+[/ -]?\d+[/ -]?\d+(?: *- *\d+)?"
+EMAIL_REGEXP = "\w+@\w+\.\w+"
+URL_REGEXP = "((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)"
+
+STOP_WORDS = get_stop_words("ru") + [
+    "январь",
+    "февраль",
+    "март",
+    "апрель",
+    "май",
+    "июнь",
+    "июль",
+    "август",
+    "сентябрь",
+    "октябрь",
+    "ноябрь",
+    "декабрь",
+]
+
+
+class NatashaProcessor:
+    def __init__(self):
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+
+        self.emb = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(self.emb)
+        self.syntax_parser = NewsSyntaxParser(self.emb)
+        self.ner_tagger = NewsNERTagger(self.emb)
+
+        self.names_extractor = NamesExtractor(self.morph_vocab)
+
+    def tag_ner(self, text):
+        doc = Doc(text)
+
+        doc.segment(self.segmenter)
+        doc.tag_ner(self.ner_tagger)
+        return [(sp.start, sp.stop, sp.text, sp.type) for sp in doc.spans]
+
+
+def find_info(text, info_type):
+    if info_type == "phone":
+        return re.findall(PHONE_REGEXP, text)
+    elif info_type == "email":
+        return re.findall(EMAIL_REGEXP, text)
+    elif info_type == "url":
+        return [r[0] for r in re.findall(URL_REGEXP, text)]
+    else:
+        return []
+
+
+def text_summary(text):
+    return summarizer.summarize(text, ratio=0.1).replace("\t", " ").split("\n")
+
+
+def text_keywords(text, stopwords=STOP_WORDS):
+    return [
+        keyword
+        for keyword in keywords.keywords(text).split("\n")
+        if keyword not in stopwords
+    ]
+
+
+app = FastAPI()
+nat_proc = NatashaProcessor()
+
+
+@app.post("/ping")
+def ping():
+    return {"status": "ok"}
+
+
+@app.post("/parse")
+def parse_text(text):
+    data_dict = {}
+
+    try:
+        for t in ["phone", "email", "url"]:
+            data_dict[t] = find_info(text, t)
+    except:
+        for t in ["phone", "email", "url"]:
+            data_dict[t] = []
+
+    try:
+        data_dict["summary"] = " ".join(text_summary(text))
+        data_dict["keywords"] = text_keywords(text)
+    except:
+        data_dict["summary"] = []
+        data_dict["keywords"] = []
+
+    try:
+        data_dict["ner"] = nat_proc.tag_ner(text)
+    except:
+        data_dict["ner"] = []
+
+    return data_dict
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+# import textract
+# https://textract.readthedocs.io/en/stable/
